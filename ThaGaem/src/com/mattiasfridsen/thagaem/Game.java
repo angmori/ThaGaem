afb18724a78d74b6ed0611cb18a5518f65828172
angmori <mattias_fridsen@hotmail.se>
2014-12-18 21:10:41 +0100

    First commit I guess
    commit commit commit

---
+++ ThaGaem/src/com/mattiasfridsen/thagaem/Game.java
@@ -0,0 +1,147 @@
+package com.mattiasfridsen.thagaem;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+
+import javax.swing.JFrame;
+
+import com.mattiasfridsen.thagaem.graphics.Screen;
+import com.mattiasfridsen.thagaem.input.Keyboard;
+
+public class Game extends Canvas implements Runnable {
+
+	private static final long serialVersionUID = 1L;
+	public	static	String	title	= "ThaGaem";
+
+	public 	static 	int width	= 300;
+	public 	static 	int height	= width / 16 * 9;
+	public 	static 	int scale	= 3;
+	
+	int	x = 0;
+	int	y = 0;	
+	
+	private Thread 		gameThread;	
+	private JFrame 		frame;	
+	private Screen 		screen;	
+	private Keyboard 	key;
+	
+	private BufferedImage image	= new BufferedImage(width, height,
+			BufferedImage.TYPE_INT_RGB);
+	
+	private int[] pixels		= 
+			((DataBufferInt)image.getRaster().getDataBuffer()).getData();
+	
+	private boolean running		= false;
+	
+	public Game() {
+		Dimension size = new Dimension(width * scale, height * scale);
+		setPreferredSize(size);
+		
+		screen	= new Screen(width, height); 
+		frame 	= new JFrame();
+		key 	= new Keyboard();
+		
+		addKeyListener(key);
+	}
+	
+	public synchronized void start() {
+		running	= true;
+		
+		gameThread = new Thread(this, "Game");
+		gameThread.start();
+	}
+	
+	public synchronized void stop() {
+		running	= false;
+		
+		try {
+			gameThread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void run() {
+		long 	lastTime 	= System.nanoTime();
+		long	timer		= System.currentTimeMillis();
+		final 	double ns	= 1000000000.0 / 60.0;
+		double 	delta 		= 0;
+		int 	frames		= 0;
+		int 	updates		= 0;
+
+		while (running) {
+			long now = System.nanoTime();
+			delta 	+= (now - lastTime) / ns;
+			lastTime = now;
+			
+			while (delta >= 1) {
+				update();
+				updates++;
+				delta--;
+			}
+			
+			render();
+			frames++;
+			
+			if (System.currentTimeMillis() - timer > 1000) {
+				timer += 1000;
+				System.out.println(updates + " UPS, " + frames + " FPS");
+				frame.setTitle(title + " | " + updates + " UPS, " + frames 
+						+ " FPS");
+				updates = 0;
+				frames 	= 0;
+			}
+		}
+		
+		stop();
+	}
+
+	public void update() {
+		key.update();
+		if (key.up) 	{ y--; }
+		if (key.down) 	{ y++; }
+		if (key.left) 	{ x--; }
+		if (key.right) 	{ x++; }
+	}
+	
+	public void render() {
+		BufferStrategy bs = getBufferStrategy();
+		if (bs == null) {
+			createBufferStrategy(3);
+			return;
+		}
+		
+		screen.clear();
+		screen.render(x, y);
+		
+		for (int i = 0; i < pixels.length; i++ ) {
+			pixels[i] = screen.pixels[i];
+		}
+		
+		Graphics g = bs.getDrawGraphics();
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, getWidth(), getHeight());
+		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
+		g.dispose();
+		bs.show();
+	}
+	
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.frame.setResizable(false);
+		game.frame.setTitle(Game.title);
+		game.frame.add(game);
+		game.frame.pack();
+		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		game.frame.setLocationRelativeTo(null);
+		game.frame.setVisible(true);
+		
+		game.start();
+	}
+}
